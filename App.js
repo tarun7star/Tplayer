68c8e89aa3ab8a1879277fd63439de26337b85a8
tarun7star <you@example.comcls>
2024-08-02 19:14:40 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,116 @@
+var song1 = document.getElementById("Song1");
+var icon1 = document.getElementById("icon1");
+
+icon1.onclick = function () {
+  if (song1.paused) {
+    song1.play();
+    icon1.src="media/pause.png";
+  } else {
+    song1.pause();
+    icon1.src="media/icon.png";
+  }
+};
+
+var song2 = document.getElementById("Song2");
+var icon2 = document.getElementById("icon2");
+
+icon2.onclick = function() {
+  if (song2.paused) {
+    song2.play();
+    icon2.src = "media/pause.png";
+  }else {
+    song2.pause();
+    icon2.src = "media/icon.png";
+  }
+}
+
+var song3 = document.getElementById("Song3");
+var icon3 = document.getElementById("icon3");
+
+icon3.onclick = function () {
+  if (song3.paused) {
+    song3.play();
+    icon3.src = "media/pause.png";
+  } else {
+    song3.pause();
+    icon3.src = "media/icon.png";
+  }
+};
+
+var song4 = document.getElementById("Song4");
+var icon4 = document.getElementById("icon4");
+
+icon4.onclick = function () {
+  if (song4.paused) {
+    song4.play();
+    icon4.src = "media/pause.png";
+  } else {
+    song4.pause();
+    icon4.src = "media/icon.png";
+  }
+};
+
+var song5 = document.getElementById("Song5");
+var icon5 = document.getElementById("icon5");
+
+icon5.onclick = function () {
+  if (song5.paused) {
+    song5.play();
+    icon5.src = "media/pause.png";
+  } else {
+    song5.pause();
+    icon5.src = "media/icon.png";
+  }
+};
+
+var song6 = document.getElementById("Song6");
+var icon6 = document.getElementById("icon6");
+
+icon6.onclick = function () {
+  if (song6.paused) {
+    song6.play();
+    icon6.src = "media/pause.png";
+  } else {
+    song6.pause();
+    icon6.src = "media/icon.png";
+  }
+};
+
+var song8 = document.getElementById("Song8");
+var icon8 = document.getElementById("icon8");
+
+icon8.onclick = function () {
+  if (song8.paused) {
+    song8.play();
+    icon8.src = "media/pause.png";
+  } else {
+    song8.pause();
+    icon8.src = "media/icon.png";
+  }
+};
+
+var song9 = document.getElementById("Song9");
+var icon9 = document.getElementById("icon9");
+
+icon9.onclick = function () {
+  if (song9.paused) {
+    song9.play();
+    icon9.src = "media/pause.png";
+  } else {
+    song9.pause();
+    icon9.src = "media/icon.png";
+  }
+};
+
+var song10 = document.getElementById("Song10");
+var icon10 = document.getElementById("icon10");
+
+icon10.onclick = function () {
+  if (song10.paused) {
+    song10.play();
+    icon10.src = "media/pause.png";
+  } else {
+    song10.pause();
+    icon10.src = "media/icon.png";
+  }
+};
